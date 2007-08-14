d5918d63fe2ccef338b614afbaa77318ff7cca74
ts <ts@4830ddd0-7b3f-de11-9cfd-0015170b777a>
2007-08-14 13:53:50 +0000

    Load bdb-0.6.2 into trunk.
    git-svn-id: file:///home/knu/repos/ts/bdb/trunk@111 4830ddd0-7b3f-de11-9cfd-0015170b777a

--- src/bdb.c
+++ src/bdb.c
@@ -512,7 +512,11 @@ Init_bdb()
 #ifdef DB_REP_ISPERM
     rb_define_const(bdb_mDb, "REP_ISPERM", INT2FIX(DB_REP_ISPERM));
     rb_define_const(bdb_mDb, "REP_NOTPERM", INT2FIX(DB_REP_NOTPERM));
+#endif
+#ifdef DB_REP_IGNORE
     rb_define_const(bdb_mDb, "REP_IGNORE", INT2FIX(DB_REP_IGNORE));
+#endif
+#ifdef DB_REP_JOIN_FAILURE
     rb_define_const(bdb_mDb, "REP_JOIN_FAILURE", INT2FIX(DB_REP_JOIN_FAILURE));
 #endif
     rb_define_const(bdb_mDb, "EID_BROADCAST", INT2FIX(DB_EID_BROADCAST));
